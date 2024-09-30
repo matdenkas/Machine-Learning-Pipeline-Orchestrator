9a015714e208bd353528724f8ab12f47859f2541
matdenkas <mateokastl@gmail.com>
2024-09-30 16:33:02 -0500

    serving front end from core

--- ml-pipe-core/src/main.py
+++ ml-pipe-core/src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/", StaticFiles(directory="static",html = True), name="static")
 
-@app.get("/")
+@app.get("/api")
 async def root():
     return {"message": "Hello World"}
