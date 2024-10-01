0d9d9e0e1f787ea9064dc37b0e9933145eae109f
matdenkas <mateokastl@gmail.com>
2024-09-30 22:48:27 -0500

    Adding @xyflow for the drag and drop

--- ml-pipe-front/src/App.js
+++ ml-pipe-front/src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from 'react';
+import {
+  ReactFlow,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  MiniMap,
+  Background,
+  Controls,
+  Panel,
+  Position,
+} from '@xyflow/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import '@xyflow/react/dist/style.css';
+
+const nodeDefaults = {
+  sourcePosition: Position.Right,
+  targetPosition: Position.Left,
+};
+
+const initialNodes = [
+  {
+    id: 'A',
+    type: 'input',
+    position: { x: 0, y: 150 },
+    data: { label: 'A' },
+    ...nodeDefaults,
+  },
+  {
+    id: 'B',
+    position: { x: 250, y: 0 },
+    data: { label: 'B' },
+    ...nodeDefaults,
+  },
+  {
+    id: 'C',
+    position: { x: 250, y: 150 },
+    data: { label: 'C' },
+    ...nodeDefaults,
+  },
+  {
+    id: 'D',
+    position: { x: 250, y: 300 },
+    data: { label: 'D' },
+    ...nodeDefaults,
+  },
+];
+
+const initialEdges = [
+  {
+    id: 'A-B',
+    source: 'A',
+    target: 'B',
+  },
+  {
+    id: 'A-C',
+    source: 'A',
+    target: 'C',
+  },
+  {
+    id: 'A-D',
+    source: 'A',
+    target: 'D',
+  },
+];
+
+const ColorModeFlow = () => {
+  const [colorMode, setColorMode] = useState('dark');
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
   );
-}
 
-export default App;
+  const onChange = (evt) => {
+    setColorMode(evt.target.value);
+  };
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      colorMode={colorMode}
+      fitView
+    >
+      <MiniMap />
+      <Background />
+      <Controls />
+
+      <Panel position="top-right">
+        <select onChange={onChange} data-testid="colormode-select">
+          <option value="dark">dark</option>
+          <option value="light">light</option>
+          <option value="system">system</option>
+        </select>
+      </Panel>
+    </ReactFlow>
+  );
+};
+
+export default ColorModeFlow;
