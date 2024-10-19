47a42583e71d681938c5ef56687ffb8eaaa837df
Beto Estrada Jr <betoejr10@yahoo.com>
2024-10-18 21:50:57 -0500

    Added cross validation by year

--- ml-pipe-trainer/src/data_parser.py
+++ ml-pipe-trainer/src/data_parser.py
@@ -1,24 +1,32 @@
 import numpy as np
+import pandas as pd
 from pandas import DataFrame
 
-def cross_validation(df: DataFrame, dataset_information: dict):
+def cross_validation(df: DataFrame, cross_validation_information: dict):
+    if cross_validation_information['type'] == 'year':
+        return split_dataset_by_year(df, cross_validation_information)
+
+
+def split_dataset_by_year(df: DataFrame, cross_validation_information: dict):
+    df.index = pd.to_datetime(df.index)
+
     dataset = {}
 
-    dataset['training'] = df[df['date'].dt.year.isin(dataset_information['trainingYears'])]
+    dataset['training'] = df[df.index.year.isin(cross_validation_information['trainingYears'])]
 
     dataset['validation'] = None
 
-    validation_years = dataset_information['validationYears']
+    validation_years = cross_validation_information['validationYears']
     if validation_years is not None:
-        dataset['validation'] = df[df['date'].dt.year.isin(validation_years)]
+        dataset['validation'] = df[df.index.year.isin(validation_years)]
 
-    dataset['testing'] = df[df['date'].dt.year.isin(dataset_information['testingYears'])]
+    dataset['testing'] = df[df.index.year.isin(cross_validation_information['testingYears'])]
 
     return dataset
 
 
 def get_XY(df, target_features):
-    y = np.array(df[target_features])
+    y = np.array(df[target_features]).ravel()
     X = np.array(df.drop(target_features, axis=1))
 
     return X, y
