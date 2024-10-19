6551d7e5baf20b4122459806cba415f92f7a39f5
Beto Estrada Jr <betoejr10@yahoo.com>
2024-10-18 21:52:51 -0500

    Added proper parsing of input csv file and job specification

--- ml-pipe-trainer/src/main.py
+++ ml-pipe-trainer/src/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, Form, UploadFile
 from io import BytesIO
+import json
 import pandas as pd
 
-from manager import run_pipeline
+from src.manager import run_pipeline
 
 app = FastAPI()
 
@@ -10,7 +11,8 @@ def bytes_to_dataframe(file: UploadFile):
     """Converts uploaded file to BytesIO stream, and then to Pandas DataFrame"""
     contents = file.file.read()
     buffer = BytesIO(contents)
-    df = pd.read_csv(buffer)
+    # NOTE:: This assumes that the first column is the index. Probably not ideal.
+    df = pd.read_csv(buffer, index_col=0)
     buffer.close()
     file.file.close()
 
@@ -21,9 +23,12 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/runjob/")
-async def run_job(data_file: UploadFile, job_specification_file: dict):
+async def run_job(data_file: UploadFile, job_specification: str = Form(...)):
+
     df_dataset = bytes_to_dataframe(data_file)
 
-    df_performance_metrics = run_pipeline(df_dataset, job_specification_file)
+    job_specification_dict = json.loads(job_specification)
 
-    return df_performance_metrics
+    df_performance_metrics = run_pipeline(df_dataset, job_specification_dict)
+
+    return df_performance_metrics.to_json(orient="records")
