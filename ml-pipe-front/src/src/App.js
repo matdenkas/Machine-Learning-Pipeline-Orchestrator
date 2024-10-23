c5d1f79c6b15d9b805230097a449ade847b98ef8
SreeSus-1 <164704978+SreeSus-1@users.noreply.github.com>
2024-10-23 10:41:14 -0500

    Add files via upload

---
+++ ml-pipe-front/src/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PipelineDesigner from './components/PipelineDesigner'; // Existing component
+import RunJob from './components/RunJob'; // New RunJob component
+import './styles.css'; // Import your CSS file
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="pipeline-designer">
+        <h1>Pipeline Designer</h1>
+        <PipelineDesigner />
+      </div>
+    </div>
+  );
+};
+
+export default App;
