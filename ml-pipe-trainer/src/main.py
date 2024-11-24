73d66ba6fe2cc40e8d85d7acbd06815d174b4de3
Beto Estrada Jr <betoejr10@yahoo.com>
2024-11-24 16:47:03 -0600

    Added CORSMiddleware, updated post_data endpoint, and cleaned code

--- ml-pipe-trainer/src/main.py
+++ ml-pipe-trainer/src/main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #----------------------------------
 # Created By : Beto Estrada
-# Created Date: 10/18/2024
-# version 1.0
+# Created Date: 10/24/2024
+# version 1.1
 #----------------------------------
 """ This file contains the different FastAPI routes
 for the trainer container.
@@ -10,17 +10,25 @@ for the trainer container.
 #----------------------------------
 # 
 #
-from fastapi import FastAPI, Form, UploadFile, Request
-from io import BytesIO
-import json
+from fastapi import FastAPI, UploadFile, Request
+from fastapi.middleware.cors import CORSMiddleware
+from io import BytesIO, StringIO
 import pandas as pd
 import requests
-import os
-import signal
 
 from src.pipeline_manager import run_pipeline
 
 app = FastAPI()
+
+# Allow CORS from all origins (*)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class worker:
     job_specification = None
     data = None
@@ -29,23 +37,10 @@ class worker:
 
 THIS_WORKER = worker()
 
-def bytes_to_dataframe(file: UploadFile):
-    """Converts uploaded file to BytesIO stream, and then to Pandas DataFrame"""
-    contents = file.file.read()
-    buffer = BytesIO(contents)
-    # NOTE:: This assumes that the first column is the index. Probably not ideal.
-    df = pd.read_csv(buffer, index_col=0)
-    buffer.close()
-    file.file.close()
-
-    return df
-
-
 @app.post("/api/init")
 async def init(request: Request):
     json = await request.json()
     THIS_WORKER.token = json['session_token']
-    print(f'MY TOKEN IS: {THIS_WORKER.token}')
 
 
 @app.post("/api/postJob/")
@@ -55,28 +50,25 @@ async def post_job(request: Request):
 
 @app.post("/api/postData/")
 async def post_data(request: Request):
-    data_file_string = await request.json()
+    form_data = await request.form()
 
-    # NOTE:: This assumes that the first column is the index. Probably not ideal.
-    df_dataset = pd.read_json(data_file_string, index_col=0)
+    # Extract the CSV content (sent as text in the "data_file" key)
+    csv_content = form_data["data_file"]
+    
+    # Convert the CSV text content into a Pandas DataFrame
+    df_data = pd.read_csv(StringIO(csv_content), index_col = 0)
 
-    print(df_dataset)
-    print(df_dataset.info())
+    THIS_WORKER.data = df_data
 
-    #df_dataset = bytes_to_dataframe(json)
+    THIS_WORKER.results = run_pipeline(df_data, THIS_WORKER.job_specification).to_json(orient="records")
 
-    THIS_WORKER.data = df_dataset
-
-    print('TRAINING FINISHED')
     requests.post('http://host.docker.internal:8000/api/postfinishedtraining/', json={'session_token': THIS_WORKER.token})
 
 
 @app.post("/runJob/")
 async def run_job():
-    # job_specification_dict = json.loads(THIS_WORKER.job_specification)
-
-    # THIS_WORKER.results = run_pipeline(df_dataset, job_specification_dict).to_json(orient="records")
-
+    # NOTE:: Currently implemented in post_data above instead. Probably should be moved here, but left there for now
+    # THIS_WORKER.results = run_pipeline(THIS_WORKER.data, THIS_WORKER.job_specification).to_json(orient="records")
     print('TRAINING FINISHED')
     requests.post('http://host.docker.internal:8000/api/postfinishedtraining/', json={'session_token': THIS_WORKER.token})
     
